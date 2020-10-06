c55a94a99c80a039222cc3debd660d64a2f91c4e
Pedro Jorge <pjls2@cin.ufpe.br>
2020-10-06 10:46:38 -0300

    inserindo respostas para o quiz de variaveis

---
+++ quizvariaveis.py
@@ -0,0 +1,81 @@
+# def f1():
+#     a=5
+#     print(a)
+
+# f1()
+
+
+# def f2():
+#     a=12
+#     b=48
+#     print(b/a +4)
+
+# f2()
+
+# def f3(a):
+#     b = a*5
+#     c= b // 2 + 9
+#     return c+b-a
+# print(f3(4))
+
+
+# def f4():
+#     a = "1"
+#     print(a)
+
+# f4()
+
+# def f5():
+#     a=1
+#     b=1.0
+#     c="1.0"
+#     print(type(a),type(b),type(c))
+
+# f5()
+
+
+# def f6(texto, numero):
+#     if len(texto) == numero:
+#         return x
+#     else:
+#         return -1
+
+# print(f6("Goblin Slayer", 12))
+
+# def f7(cateto1, cateto2):
+#     resultado = (cateto1**2 + cateto2**2)**(1/2)
+#     return hipotenusa
+
+# print(f7(9,12))
+
+
+# def f8(a,b):
+#     resultado = 1
+#     if a>b:
+#         resultado=-1
+#     elif a==b:
+#         resultado = 0
+#     return resultado
+
+# f8(123, 44)
+
+# print(resultado)
+
+
+# maior = 100
+
+# def f9(x, y):
+#     maior=x
+#     if y>x:
+#         maior =y
+#     return maior
+
+# print(f9(10,20))
+# print(maior)
+
+def f10(a):
+    b=a*5
+    b=b+1
+    return b
+
+print(f10(4))
