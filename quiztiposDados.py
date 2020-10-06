7e8c546836d438b8e3f17ba8fc1a14c93e08bf80
Pedro Jorge <pjls2@cin.ufpe.br>
2020-10-06 10:12:33 -0300

    Questões do quiz de tipos de dados

---
+++ quiztiposDados.py
@@ -0,0 +1,66 @@
+# a=2.35
+# print(int(a)) #2
+
+
+# a=2.35
+# print(int("a")) #erro
+
+# a = type(32//9)
+# print(a) #<class 'int'>
+
+# print(3 and 0) # 0
+
+# print(0 and 3) # 0
+
+# print(0 or 3) # 3
+
+# a:int = input("primeiro cateto")
+# b:int = input("segundo cateto")
+
+# def pitagoras (a:int, b:int) -> int:
+#     return (a*a) + (b*b)
+
+# c:int = pitagoras(a,b) #erro
+
+# pi = 3.14
+# def areaBase(r:int, pi:int)->int:
+#     return 2*pi*(r*r)
+
+# def areaLateral(r:int, h:int, pi:int)->int:
+#     return 2*pi*r*h
+
+# def areaCilindro(r:int, h:int, pi:int)->int:
+#     return areaBase(r, pi)+areaLateral(r,h,pi)
+
+# print(areaCilindro(2,10,pi))
+
+# a=True
+# b=True
+# c=False
+# exp=not(a and b or c)
+
+# print(exp)
+
+
+
+# a=True
+# b=False
+# c=True
+# exp=a and c or c and b
+
+# print(exp)
+
+# a=False
+# b=True
+# c=False
+
+# if b:
+#     print("dog")
+# if a and b:
+#     print("gato")
+# elif a or b:
+#     print("passarinho")
+# if not(a and c)
+#     print ("peixe")
+# else:
+#     print("pinguin")
